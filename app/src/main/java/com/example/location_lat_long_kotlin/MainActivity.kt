1b028f597c2c7b004a8ef132107c9bb988dfab0a
PAULI <adenreleademola@yahoo.com>
2019-06-12 18:54:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/location_lat_long_kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.location_lat_long_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
