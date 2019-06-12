6b18f91825f4d2198d3b9b44757fe9ec427af2a7
PAULI <adenreleademola@yahoo.com>
2019-06-12 18:57:33 +0100

    Declaring variables

--- app/src/main/java/com/example/location_lat_long_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/location_lat_long_kotlin/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.location_lat_long_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 
 class MainActivity : AppCompatActivity() {
-    
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
